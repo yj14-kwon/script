40f596811993ef2b50b11c2a71b859be1bccbb9a
yj14-kwon <75778501+yj14-kwon@users.noreply.github.com>
2021-07-22 17:41:15 +0900

    Update add_user_test.sh

--- linux/add_user_test.sh
+++ linux/add_user_test.sh
@@ -1,4 +1,4 @@
-RESULT=$(useradd -p `openssl passwd -1 'q1w2e3R$'` -m -s /bin/bash test_user)$?
+RESULT=$(useradd -p `openssl passwd -1 '{{password}}'` -m -s /bin/bash {{username}})$?
 
 if [ "$RESULT" -eq 0 ]; then
   echo "The test_user account has been created."
